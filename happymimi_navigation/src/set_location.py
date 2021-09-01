Fix: quaternionの型変換部分を変更

happymimi_navigation/src/set_location.py
@@ -9,6 +9,7 @@ import rospy
 import rosparam
 import roslib.packages
 import tf
+from geometry_msgs.msg import Quaternion
 from tf import TransformListener
 from nav_msgs.msg import Odometry
 from happymimi_navigation.srv import SetLocation, SetLocationResponse
@@ -40,14 +41,14 @@ class SetLocationServer():
         # t = self.tf.getLatestCommonTime("/map", "/odom")
         position, quaternion = self.tf.lookupTransform("/map", "/base_link", rospy.Time(0))
         q = tf.transformations.quaternion_from_euler(position[0], position[1], position[2])
-        self.location_pose_x = q[0]
-        self.location_pose_y = q[1]
-        self.location_pose_z = q[2]
-        self.location_pose_w = q[3]
-        # self.location_pose_x = position[0]
-        # self.location_pose_y = position[1]
-        # self.location_pose_z = quaternion[2]
-        # self.location_pose_w = quaternion[3]
+        # self.location_pose_x = float(q[0])
+        # self.location_pose_y = float(q[1])
+        # self.location_pose_z = float(q[2])
+        # self.location_pose_w = float(q[3])
+        self.location_pose_x = position[0]
+        self.location_pose_y = position[1]
+        self.location_pose_z = quaternion[2]
+        self.location_pose_w = quaternion[3]
 
     def checkState(self, srv_req):
         if srv_req.state == 'add':