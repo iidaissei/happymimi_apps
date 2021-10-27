739814d3672bcaec3b8ac488abfe6af36b8db943
HappyYusuke <c1005073@planet.kanazawa-it.ac.jp>
2021-10-27 19:26:31 +0900

    add enter_room pkg

--- enter_room/src/enter_server.py
+++ enter_room/src/enter_server.py
@@ -8,20 +8,28 @@
 # サービスで「進む距離」と「速度」を指定できる
 #-------------------------------------------------------------
 import rospy
+import roslib
+import sys
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
-from happymimi_teleop.srv import EnterRoomGen2, EnterRoomGen2Response
+from enter_room.srv import EnterRoom, EnterRoomResponse
+from happymimi_voice_msgs.srv import TTS
+file_path = roslib.packages.get_pkg_dir('happymimi_teleop') + '/src/'
+sys.path.insert(0, file_path)
+from base_control import BaseControl
+
 
 # 速度と進む距離を取得
 # 
 class EnterRoomServer():
     def __init__(self):
-        service = rospy.Service('/enter_room_server', EnterRoomGen2, self.execute)
+        service = rospy.Service('/enter_room_server', EnterRoom, self.execute)
         rospy.loginfo("Ready to set enter_room_server")
-        # Publisher
-        self.twist_pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
+        # speak
+        self.tts_srv = rospy.ServiceProxy('/tts', TTS)
         # Subscriber
         rospy.Subscriber('/scan', LaserScan, self.laserCB)
+        # Module
+        self.base_control = BaseControl()
         # Value
         self.front_laser_dist = 999.9
 
@@ -29,33 +37,20 @@ class EnterRoomServer():
         self.front_laser_dist = receive_msg.ranges[359]
 
     def execute(self, srv_req):
-        # Value
-        vel = Twist()
-        vel.linear.x = srv_req.velocity
-        target_dist = srv_req.distance
-        safe_dist = 1.0
-        target_time = (self.front_laser_dist + target_dist) / vel.linear.x # 速度と進行距離から実行時間を計算
-        start_time = rospy.get_time() # 実行開始時間を格納
+        try:
+            safe_dist = 1.0
+            target_dist = srv_req.distance + self.front_laser_dist
 
-        while not rospy.is_shutdown():
-            if self.front_laser_dist >= safe_dist and (rospy.get_time() - start_time) <= target_time: # 安全距離かつ実行時間内のとき実行
-                #print "now_time = ", rospy.get_time() - start_time #経過時間確認用
+            #self.tts_srv('Prease open the door')
+            print "Prease open the door"
+            while self.front_laser_dist <= safe_dist:
                 rospy.sleep(0.1)
-                self.twist_pub.publish(vel)
-            elif self.front_laser_dist <= safe_dist: # 障害物があるとき実行
-                print "Please open the door"
-                rospy.sleep(3.0)
-            else: # megaroverを停止して、終了時刻を格納してbreak
-                vel.linear.x = 0.0
-                self.twist_pub.publish(vel)
-                finish_time = rospy.get_time() - start_time
-                break
-            
-        if finish_time >= target_time:
-            print "enter_room finish [distance:", srv_req.distance, "velocity:", srv_req.velocity, "]"
-            return EnterRoomGen2Response(result = True)
-        else:
-            return EnterRoomGen2Response(result = False)
+            #self.tts_srv('Thank you')
+            self.base_control.translateDist(target_dist, srv_req.velocity)
+            return EnterRoomResponse(result = True)
+        except rospy.ROSInterruptException:
+            rospy.logger("!!Interrupt!!")
+            return EnterRoomResponse(result = False)
 
 if __name__ == '__main__':
     rospy.init_node('enter_server')
