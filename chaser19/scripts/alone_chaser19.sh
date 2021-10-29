525ae3ac0dc3e45b2575543f607c61ebb1fa7976
iidaissei <issei.288.higashi@icloud.com>
2021-10-29 10:32:54 +0900

    Fix: 軽微な修正

--- chaser19/scripts/alone_chaser19.sh
+++ chaser19/scripts/alone_chaser19.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-xterm -geometry 80x5+0+0   -e "/opt/ros/kinetic/bin/roslaunch turtlebot_bringup minimal.launch" &
-sleep 10s
-xterm -geometry 80x5+0+130 -e "/opt/ros/kinetic/bin/roslaunch turtlebot_bringup 3dsensor.launch" &
+xterm -geometry 80x5+0+0   -e "/opt/ros/melodic/bin/roslaunch happymimi_bringup minimal.launch" &
 sleep 5s
-xterm -geometry 80x5+0+260 -e "/opt/ros/kinetic/bin/rqt -s kobuki_dashboard" &
+xterm -geometry 80x5+0+130 -e "/opt/ros/melodic/bin/roslaunch happymimi_bringup sensor.launch" &
 sleep 5s
-xterm -geometry 80x5+0+360 -e "~/catkin_ws/devel/lib/chaser19/chaser19" &
+xterm -geometry 80x5+0+260 -e "~/catkin_ws/devel/lib/chaser19/chaser19" &
 sleep 5s
-xterm -geometry 80x5+0+910 -e "/opt/ros/kinetic/bin/rostopic pub /follow_human std_msgs/String start" &
+xterm -geometry 80x5+0+360 -e "/opt/ros/melodic/bin/rostopic pub /follow_human std_msgs/String start" &
 sleep 1s
 #xterm -geometry 80x5+0+460 -e "/opt/ros/kinetic/bin/rosbag record -a" &
 #sleep 5s
--- chaser19/scripts/chaser19.sh
+++ chaser19/scripts/chaser19.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-xterm -geometry 80x5+0+0   -e "/opt/ros/kinetic/bin/roslaunch turtlebot_bringup minimal.launch" &
-sleep 10s
-xterm -geometry 80x5+0+130 -e "/opt/ros/kinetic/bin/roslaunch turtlebot_bringup 3dsensor.launch" &
+xterm -geometry 80x5+0+0   -e "/opt/ros/melodic/bin/roslaunch happymimi_bringup minimal.launch" &
 sleep 5s
-xterm -geometry 80x5+0+260 -e "/opt/ros/kinetic/bin/rqt -s kobuki_dashboard" &
+xterm -geometry 80x5+0+130 -e "/opt/ros/melodic/bin/rosrun urg_node urg_node" &
 sleep 5s
 #xterm -geometry 80x5+0+360 -e "/opt/ros/kinectic/bin/rosbag record -a" &
 #sleep 5s
-xterm -geometry 80x5+0+910 -e "~/catkin_ws/devel/lib/chaser19/chaser19" &
-
-
+xterm -geometry 80x5+0+910 -e "~/test_ws/devel/lib/chaser19/chaser19"
