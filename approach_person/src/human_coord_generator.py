abca436441f5f1862e24f4e355ed0d37e6d97c66
iidaissei <issei.288.higashi@icloud.com>
2021-11-10 20:44:38 +0900

    Fix: 実機でのデバッグやったよ

--- approach_person/src/human_coord_generator.py
+++ approach_person/src/human_coord_generator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import rospy
+import roslib
 import rosparam
 import tf
 import tf2_ros
@@ -8,13 +9,13 @@ import actionlib
 import geometry_msgs.msg
 
 from approach_person.msg import PubHumanTFAction, PubHumanTFGoal
-from happymimi_msgs.srv import SimpleTrg
+from happymimi_msgs.srv import SimpleTrg, SimpleTrgResponse
 
 class GenerateHumanCoord():
     def __init__(self):
         self.sac = actionlib.SimpleActionClient('pub_human_tf', PubHumanTFAction)
         self.sac.wait_for_server()
-        slef.goal = PubHumanTFGoal()
+        self.goal = PubHumanTFGoal()
 
         self.tfBuffer = tf2_ros.Buffer()
         self.listener = tf2_ros.TransformListener(self.tfBuffer)
@@ -29,12 +30,12 @@ class GenerateHumanCoord():
         self.sac.send_goal(self.goal)
         while not rospy.is_shutdown():
             try:
-                trans = tfBuffer.lookup_transform('map', frame_name, rospy.Time())
+                trans = self.tfBuffer.lookup_transform('map', frame_name, rospy.Time())
+                self.human_dict[frame_name] = []
                 self.human_dict[frame_name].append(trans.transform.translation.x)
                 self.human_dict[frame_name].append(trans.transform.translation.y)
                 self.human_dict[frame_name].append(trans.transform.rotation.z)
                 self.human_dict[frame_name].append(trans.transform.rotation.w)
-                print self.human_dict
                 break
             except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
                 self.rate.sleep()
@@ -48,22 +49,23 @@ class HumanCoordGeneratorSrv():
     def __init__(self):
         hcg_srv = rospy.Service('human_coord_generator', SimpleTrg, self.execute)
         rospy.loginfo("Ready to human_coord_generator server")
-        self.ghTF = GenerateHumanTF()
         self.ghc = GenerateHumanCoord()
         self.human_coord_dict = {}
 
     def execute(self, srv_req):
-        dist_data = srv_req.dist_list
-        for i range(dist_data):
+        dist_data = [[0.5, 0.5], [0.7, -0.5], [1.0, 0.0]]
+        for i in range(len(dist_data)):
+        # for i in dist_data:
             frame_id = "human_" + str(i)
             # map座標系に変換してlocation dictを作成
-            human_coord = self.ghTF.execute(frame_id, dist_data[i][0], dist_data[i][1])
+            human_coord = self.ghc.execute(frame_id, dist_data[i][0], dist_data[i][1])
             self.human_coord_dict.update(human_coord)
             # dict はパラメータとして保存する
 
         param_path = roslib.packages.get_pkg_dir("approach_person")
         rospy.set_param('/human_location', self.human_coord_dict)
         rosparam.dump_params(param_path + '/config/'  + 'human_location.yaml', '/human_location')
+        print self.human_coord_dict
         return SimpleTrgResponse(result = True)
 
 
--- approach_person/src/pub_human_tf.py
+++ approach_person/src/pub_human_tf.py
@@ -34,27 +34,14 @@ class PubHumanTFAS():
         while not rospy.is_shutdown():
             # Run this loop at about 10Hz
             rospy.sleep(0.1)
-
-            # self.t.header.frame_id = "camera_depth_frame"
             self.t.header.stamp = rospy.Time.now()
-            # self.t.child_frame_id = self.child_name
-            # この下にオフセットの値を追加する
-            # self.t.transform.translation.x = self.dist_x
-            # self.t.transform.translation.y = self.dist_y
-            # self.t.transform.translation.z = 0.0
-            #
-            # self.t.transform.rotation.x = 0.0
-            # self.t.transform.rotation.y = 0.0
-            # self.t.transform.rotation.z = 0.0
-            # self.t.transform.rotation.w = 1.0
-
             tfm = tf2_msgs.msg.TFMessage([self.t])
             self.pub_tf.publish(tfm)
             if self.sas.is_preempt_requested():
                 self.sas.set_preempted()
                 break
-        self.result = True
-        self.sas.set_succeeded(self.result)
+        # self.result = True
+        # self.sas.set_succeeded(self.result)
 
 
 if __name__ == '__main__':
